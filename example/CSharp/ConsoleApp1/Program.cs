dd28deadd9b5a5468978551f83112612a69fa1fd
alin <alin_su@tmytek.com>
2020-02-14 19:27:14 +0800

    Support NGK mAip command

--- example/CSharp/ConsoleApp1/Program.cs
+++ example/CSharp/ConsoleApp1/Program.cs
@@ -15,8 +15,10 @@ namespace ConsoleApp1
     }
     enum TRMODE
     {
-        TX = 0,
-        RX = 1,
+        STANDBY = 0,
+        TX = 1,
+        RX = 2,
+        SLEEP = 3,
     };
     class Program
     {
@@ -36,12 +38,17 @@ namespace ConsoleApp1
 
             /* It will send the init command to BBox. */
             /* first BBoxOne */
-            string s_info_1 = b.Init("B19312200-24", 0);
+            /*****************************
+                dev_type = 0, IDT device
+                dev_type = 1, KDDI device
+                dev_type = 2, NGK device
+            *****************************/
+            string s_info_1 = b.Init("B19312300-24", 2, 0);
             Console.WriteLine(s_info_1);
             Console.WriteLine("Init first one");
 
-            double dev_1_spacing = b.selectAntenna(Device.AntennaType.FOURBYFOUR, 0);
-            Console.WriteLine("1st device antenna spacing : " + dev_1_spacing);
+            //double dev_1_spacing = b.selectAntenna(Device.AntennaType.FOURBYFOUR, 0);
+            //Console.WriteLine("1st device antenna spacing : " + dev_1_spacing);
             b.SwitchTxRxMode((int)TRMODE.TX, 0);
 
 #if MULTIPLEBBOXONE
@@ -58,14 +65,18 @@ namespace ConsoleApp1
           
             int deg = 0;
             int dir = -1;
-            for(int count = 0; count < 1000; count++)
+            for(int count = 0; count < 10; count++)
             {
                 try
                 {
+                    //setRxGainPhaseStep(int icth/*1-16*/, int PH1, int PH2, int PH3, int PH4, int GA1, int GA2, int GA3, int GA4, int GAC, int idx)
+                    //b.setRxGainPhaseStep(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+                    b.setBeamXY(100.0, 0, 0, 0);
+#if IDT
                     s_info_1 = b.setBeamX(5/*dB*/, deg/*degree*/, 0);
                     s_info_1 = b.setBeamY(5/*dB*/, deg/*degree*/, 0);
                     s_info_1 = b.setBeamXY(5/*dB*/, deg/*degree*/, deg/*degree*/, 0);
-                    Console.WriteLine("Firsr Device control : " + s_info_1);
+                    Console.WriteLine("First Device control : " + s_info_1);
 #if MULTIPLEBBOXONE
           
                     s_info_2 = b.setBeamX(0, deg, 1);
@@ -79,10 +90,12 @@ namespace ConsoleApp1
                     else if (deg >= 25)
                         dir = -1;
                     deg += dir;
+#endif
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("EEROR : " + e.ToString());
+                    break;
                 }
             }
             long end_milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
